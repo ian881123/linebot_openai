098f48c637cde0fc326cac92ce81884f91c6636a
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-02-27 11:51:07 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -34,7 +34,7 @@ client.files.create(
 client.files.list()
 
 # 創建 fine-tuning 作業
-client.fine_tuning.jobs.create(training_file="file-5W3w84z70hrPoE0v8BRzQfSP", model="gpt-3.5-turbo-0125")
+client.fine_tuning.jobs.create(training_file="file-kf1nPv58Kl9lW6302ppLN4EL", model="gpt-3.5-turbo-1106")
 
 # 列出 fine-tuning 作業
 client.fine_tuning.jobs.list(limit=10)
@@ -47,7 +47,7 @@ client.fine_tuning.jobs.list_events(id="ftjob-trytry123456", limit=10)
 
 # 創建聊天完成
 completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125",
+  model="ft:gpt-3.5-turbo-1106",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
     {"role": "user", "content": "什麼是陸軍軍官學校學生學則?"}
@@ -58,7 +58,7 @@ print(completion.choices[0].message.content)
 
 # 創建帶有 fine-tuned 模型的聊天完成
 completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:personal::trytry456789",
+  model="ft:gpt-3.5-turbo-1106:personal::trytry456789",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
     {"role": "user", "content": "什麼是陸軍軍官學校學生學則?"}
@@ -70,7 +70,7 @@ print(completion2.choices[0].message.content)
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:personal::trytry456789",
+        model="ft:gpt-3.5-turbo-1106:personal::trytry456789",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
