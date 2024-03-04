c631f32de7d0789e15526aa115c99157caa3965e
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-04 08:27:06 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -17,14 +17,14 @@ import string
 os.system('pip install openai --upgrade')
 
 # 使用 curl 下載 rocma_qa.json 文件
-os.system('curl -o 1.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/1.json')
+os.system('curl -o testdata1.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/testdata1.json')
 
 # 定義客戶端
 client = OpenAI() 
 
 # 創建 fine-tune 文件
 client.files.create(
-  file=open("1.json", "rb"),
+  file=open("testdata1.json", "rb"),
   purpose='fine-tune'
 )
 
@@ -38,10 +38,10 @@ client.fine_tuning.jobs.create(training_file="file-kf1nPv58Kl9lW6302ppLN4EL", mo
 client.fine_tuning.jobs.list(limit=10)
 
 # 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-trytry123456")
+client.fine_tuning.jobs.retrieve("ftjob-kVp89uTKEM8wera127BZHleo")
 
 # 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(id="ftjob-trytry123456", limit=10)
+client.fine_tuning.jobs.list_events(id="ftjob-kVp89uTKEM8wera127BZHleo", limit=10)
 
 # 創建聊天完成
 completion = client.chat.completions.create(
@@ -56,7 +56,7 @@ print(completion.choices[0].message.content)
 
 # 創建帶有 fine-tuned 模型的聊天完成
 completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:personal::trytry456789",
+  model="ft:gpt-3.5-turbo-0125:personal::8yiGjU3J",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
     {"role": "user", "content": "什麼是陸軍軍官學校學生學則?"}
@@ -68,7 +68,7 @@ print(completion2.choices[0].message.content)
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:personal::trytry456789",
+        model="ft:gpt-3.5-turbo-0125:personal::8yiGjU3J",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
