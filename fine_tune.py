223df68ebbf3f7d3877943e2aa3e59f1f6d46299
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-04 11:19:01 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -17,67 +17,13 @@ import string
 os.system('pip install openai --upgrade')
 
 # 使用 curl 下載 rocma_qa.json 文件
-os.system('curl -o testdata1.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/testdata1.json')
+os.system('curl -o 1.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/1.json')
 
 # 定義客戶端
 client = OpenAI() 
 
 # 創建 fine-tune 文件
 client.files.create(
-  file=open("testdata1.json", "rb"),
+  file=open("1.json", "rb"),
   purpose='fine-tune'
 )
-
-# 列出文件
-client.files.list()
-
-# 創建 fine-tuning 作業
-client.fine_tuning.jobs.create(training_file="file-Cl99RRGc6e2HEhI4h31Ujw1L", model="ft:gpt-3.5-turbo-0125:personal::8yiGjU3J")
-
-# 列出 fine-tuning 作業
-client.fine_tuning.jobs.list(limit=10)
-
-# 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-kVp89uTKEM8wera127BZHleo")
-
-# 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-kVp89uTKEM8wera127BZHleo", limit=10)
-
-# 創建聊天完成
-completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125",
-  messages=[
-    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "什麼是陸軍軍官學校學生學則?"}
-  ]
-)
-
-print(completion.choices[0].message.content)
-
-# 創建帶有 fine-tuned 模型的聊天完成
-completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:personal::8yiGjU3J",
-  messages=[
-    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "什麼是陸軍軍官學校學生學則?"}
-  ]
-)
-
-print(completion2.choices[0].message.content)
-
-# 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
-def GPT_response(text):
-    response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:personal::8yiGjU3J",
-        messages=[
-            {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-            {"role": "user", "content": text}
-        ]
-    )
-
-    answer = response.choices[0].message.content
-
-    # 去除回复文本中的標點符號
-    answer = answer.translate(str.maketrans('', '', string.punctuation))
-
-    return answer
