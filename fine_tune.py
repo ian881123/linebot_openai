b90ad365846d7217ef94a0c148efc608c88a1946
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-05 23:37:49 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -28,7 +28,7 @@ client.files.list()
 # 創建 fine-tuning 作業
 client.fine_tuning.jobs.create(
   training_file="file-7n5Ofb1gV90R76SqVLthdsVs", 
-  model="ft:gpt-3.5-turbo-0125:personal::8zQdxT6b", 
+  model="ft:gpt-3.5-turbo-0125:personal::8zQwGxZk", 
   hyperparameters={
     "n_epochs":7
   }
@@ -38,17 +38,17 @@ client.fine_tuning.jobs.create(
 client.fine_tuning.jobs.list(limit=10)
 
 # 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-24nVsVNu9LYugIXifFqzrysL")
+client.fine_tuning.jobs.retrieve("ftjob-65nHjhG639C3sChSephm8MXv")
 
 # 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-24nVsVNu9LYugIXifFqzrysL", limit=10)
+client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-65nHjhG639C3sChSephm8MXv", limit=10)
 
 # 創建帶有 fine-tuned 模型的聊天完成
 completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:personal::8zQdxT6b",
+  model="ft:gpt-3.5-turbo-0125:personal::8zQwGxZk",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "宣布當週獎懲的時間為何?"}
+    {"role": "user", "content": "留讀的標準有哪些?"}
   ]
 )
 
@@ -57,7 +57,7 @@ print(completion.choices[0].message.content)
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:personal::8zQdxT6b",
+        model="ft:gpt-3.5-turbo-0125:personal::8zQwGxZk",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
