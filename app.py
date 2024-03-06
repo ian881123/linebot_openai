6b97655da8cda66ce5f0f2db14d0bb78be35b540
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-06 21:39:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,17 @@ def handle_message(event):
         GPT_answer = GPT_response(msg)
         print(GPT_answer)
         line_bot_api.reply_message(event.reply_token, TextSendMessage(GPT_answer))
-        
+
+@handler.add(MemberJoinedEvent)
+def welcome(event):
+    uid = event.joined.members[0].user_id
+    gid = event.source.group_id
+    profile = line_bot_api.get_group_member_profile(gid, uid)
+    name = profile.display_name
+    message = TextSendMessage(text=f'{name}歡迎加入')
+    line_bot_api.reply_message(event.reply_token, message)
+
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
