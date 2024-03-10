fe30a03c2bbd2cd70a06cd48089f58e42a7d0f0b
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-10 09:06:01 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -17,7 +17,7 @@ import string
 os.system('pip install openai --upgrade')
 
 # 使用 curl 下載 rocma_qa.json 文件
-os.system('curl -o 13.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/13.json')
+os.system('curl -o 13.5.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/13.5.json')
 
 
 # 定義客戶端
@@ -26,7 +26,7 @@ client = OpenAI()
 
 # 創建 fine-tune 文件
 client.files.create(
-  file=open("13.json", "rb"),
+  file=open("13.5.json", "rb"),
   purpose='fine-tune'
 )
 
