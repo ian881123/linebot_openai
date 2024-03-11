9c0790615c79824b1c27db0dd787a8b66db8dbcd
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-11 22:29:42 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -24,8 +24,8 @@ client.files.list()
 
 # 創建 fine-tuning 作業
 client.fine_tuning.jobs.create(
-  training_file="file-gNRCiSaVljSV722SHAUEl6IB", 
-  model="ft:gpt-3.5-turbo-0125:personal::91adv7f5", 
+  training_file="file-VmFT57MQ4vwRbtp2jDJ708mN", 
+  model="ft:gpt-3.5-turbo-0125:personal::91arOlXm", 
   hyperparameters={
     "n_epochs":7
   }
@@ -35,17 +35,17 @@ client.fine_tuning.jobs.create(
 client.fine_tuning.jobs.list(limit=10)
 
 # 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-CulXQWfxWMo4FgHdXeuF6FmL")
+client.fine_tuning.jobs.retrieve("ftjob-1tbnO086k31KKhEgccE2FmTA")
 
 # 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-CulXQWfxWMo4FgHdXeuF6FmL", limit=10)
+client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-1tbnO086k31KKhEgccE2FmTA", limit=10)
 
 # 創建聊天完成
 completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:personal::91WFQFLP",
+  model="ft:gpt-3.5-turbo-0125:personal::91adv7f5",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "迷彩大夾克的職務臂章要貼在年級臂槓的什麼位置?"}
+    {"role": "user", "content": "請問榮譽假的核發標準是什麼?"}
   ]
 )
 
@@ -53,10 +53,10 @@ print(completion.choices[0].message.content)
 
 # 創建帶有 fine-tuned 模型的聊天完成
 completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:personal::91adv7f5",
+  model="ft:gpt-3.5-turbo-0125:personal::91arOlXm",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "迷彩大夾克的職務臂章要貼在年級臂槓的什麼位置?"}
+    {"role": "user", "content": "請問榮譽假的核發標準是什麼?"}
   ]
 )
 
@@ -65,7 +65,7 @@ print(completion2.choices[0].message.content)
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:personal::91adv7f5",
+        model="ft:gpt-3.5-turbo-0125:personal::91arOlXm",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
