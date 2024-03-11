f785057b02fe4638de8343fe1b13a44b5ec6e528
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-11 15:50:54 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -19,6 +19,49 @@ os.system('pip install openai --upgrade')
 # 定義客戶端
 client = OpenAI() 
 
+# 列出文件
+client.files.list()
+
+# 創建 fine-tuning 作業
+client.fine_tuning.jobs.create(
+  training_file="file-ouRVxQFmBAtCOXdKCT24lCBs", 
+  model="ft:gpt-3.5-turbo-0125:personal::91UHetr6", 
+  hyperparameters={
+    "n_epochs":7
+  }
+)
+
+# 列出 fine-tuning 作業
+client.fine_tuning.jobs.list(limit=10)
+
+# 檢索 fine-tuning 作業事件
+client.fine_tuning.jobs.retrieve("ftjob-ntgzee19FARkANfIkbbUQbPI")
+
+# 列出 fine-tuning 作業事件
+client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-ntgzee19FARkANfIkbbUQbPI", limit=10)
+
+# 創建聊天完成
+completion = client.chat.completions.create(
+  model="ft:gpt-3.5-turbo-0125:personal::91TztiFX",
+  messages=[
+    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
+    {"role": "user", "content": "學生著校服時有什麼禁止事項?"}
+  ]
+)
+
+print(completion.choices[0].message.content)
+
+# 創建帶有 fine-tuned 模型的聊天完成
+completion2 = client.chat.completions.create(
+  model="ft:gpt-3.5-turbo-0125:personal::91UHetr6",
+  messages=[
+    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
+    {"role": "user", "content": "學生著校服時有什麼禁止事項?"}
+  ]
+)
+
+print(completion2.choices[0].message.content)
+
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
