31f93f332e3ed889ce83b1a7c57a9d14006bf1d0
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-13 06:12:59 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -24,7 +24,7 @@ client.files.list()
 # 創建 fine-tuning 作業
 client.fine_tuning.jobs.create(
   training_file="file-O8h2en23zswFTj9rTDrmjKBP", 
-  model="ft:gpt-3.5-turbo-0125:rocmacis::91xr7ijt", 
+  model="ft:gpt-3.5-turbo-0125:rocmacis::91yaydWG", 
   hyperparameters={
     "n_epochs":7
   }
@@ -34,17 +34,17 @@ client.fine_tuning.jobs.create(
 client.fine_tuning.jobs.list(limit=10)
 
 # 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-b7z6L1AYDEluScXjzn641gxz")
+client.fine_tuning.jobs.retrieve("ftjob-PPvoKJaPaAsY21k7TNUgC2ce")
 
 # 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-b7z6L1AYDEluScXjzn641gxz", limit=10)
+client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-PPvoKJaPaAsY21k7TNUgC2ce", limit=10)
 
 # 創建聊天完成
 completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::91wAl0vV",
+  model="ft:gpt-3.5-turbo-0125:rocmacis::91xr7ijt",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "陸軍軍官學校學生學則的法源為何?"}
+    {"role": "user", "content": "陸軍軍官學校學生員額由誰核定?"}
   ]
 )
 
@@ -52,10 +52,10 @@ print(completion.choices[0].message.content)
 
 # 創建帶有 fine-tuned 模型的聊天完成
 completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::91xr7ijt",
+  model="ft:gpt-3.5-turbo-0125:rocmacis::91yaydWG",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "陸軍軍官學校學生學則的法源為何?"}
+    {"role": "user", "content": "陸軍軍官學校學生員額由誰核定?"}
   ]
 )
 
@@ -65,7 +65,7 @@ print(completion2.choices[0].message.content)
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:rocmacis::91xr7ijt",
+        model="ft:gpt-3.5-turbo-0125:rocmacis::91yaydWG",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
