159c3b8a2fd51671961f09e6c12fcab3372ce5e7
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-12 21:34:11 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -15,56 +15,22 @@ import string
 # 安裝或升級 openai 庫
 os.system('pip install openai --upgrade')
 
+# 使用 curl 下載 rocma_qa.json 文件
+os.system('curl -o Book.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/Book.json')
+
 # 定義客戶端
 client = OpenAI() 
 
-# 列出文件
-client.files.list()
-
-# 創建 fine-tuning 作業
-client.fine_tuning.jobs.create(
-  training_file="file-XzOWUthY4LEue4Fb3FSdtcjl", 
-  model="ft:gpt-3.5-turbo-0125:rocmacis::91vcsfog", 
-  hyperparameters={
-    "n_epochs":7
-  }
+# 創建 fine-tune 文件
+client.files.create(
+  file=open("Book.json", "rb"),
+  purpose='fine-tune'
 )
-
-# 列出 fine-tuning 作業
-client.fine_tuning.jobs.list(limit=10)
-
-# 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-YqMMlKWf4izWjhOIIRKVCn2F")
-
-# 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-YqMMlKWf4izWjhOIIRKVCn2F", limit=10)
-
-# 創建聊天完成
-completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::91ux1LPt",
-  messages=[
-    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "在陸軍軍官學校，出校時下身應著什麼?"}
-  ]
-)
-
-print(completion.choices[0].message.content)
-
-# 創建帶有 fine-tuned 模型的聊天完成
-completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::91vcsfog",
-  messages=[
-    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "在陸軍軍官學校，出校時下身應著什麼?"}
-  ]
-)
-
-print(completion2.choices[0].message.content)
 
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:rocmacis::91vcsfog",
+        model="ft:gpt-3.5-turbo-0125:rocmacis::91wAl0vV",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
