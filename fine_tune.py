7e11e4a3596980996373777daa18bed625b4614f
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-13 21:18:58 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -25,7 +25,7 @@ client.files.list()
 # 創建 fine-tuning 作業
 client.fine_tuning.jobs.create(
   training_file="file-fhRVWnAegDZWo3fhyx0d8vk8", 
-  model="ft:gpt-3.5-turbo-0125:rocmacis::92HFVLoz", 
+  model="ft:gpt-3.5-turbo-0125:rocmacis::92IPAV8Z", 
   hyperparameters={
     "n_epochs":7
   }
@@ -35,17 +35,17 @@ client.fine_tuning.jobs.create(
 client.fine_tuning.jobs.list(limit=10)
 
 # 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-P5pHxzs4cATs6yfb1r14sBPq")
+client.fine_tuning.jobs.retrieve("ftjob-8z8cbaCuQns915xnxulVMlIu")
 
 # 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-P5pHxzs4cATs6yfb1r14sBPq", limit=10)
+client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-8z8cbaCuQns915xnxulVMlIu", limit=10)
 
 # 創建聊天完成
 completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::92FeMv9N",
+  model="ft:gpt-3.5-turbo-0125:rocmacis::92HFVLoz",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "陸軍軍官學校的校訓為何?"}
+    {"role": "user", "content": "陸軍軍官學校在哪裡?"}
   ]
 )
 
@@ -53,10 +53,10 @@ print(completion.choices[0].message.content)
 
 # 創建帶有 fine-tuned 模型的聊天完成
 completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::92HFVLoz",
+  model="ft:gpt-3.5-turbo-0125:rocmacis::92IPAV8Z",
   messages=[
     {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "陸軍軍官學校的校訓為何?"}
+    {"role": "user", "content": "陸軍軍官學校在哪裡?"}
   ]
 )
 
@@ -67,7 +67,7 @@ print(completion2.choices[0].message.content)
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:rocmacis::92HFVLoz",
+        model="ft:gpt-3.5-turbo-0125:rocmacis::92IPAV8Z",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
