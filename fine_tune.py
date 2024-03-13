939761b0f27816e7d4d9a98853c4db4750c174a2
ian881123 <145948200+ian881123@users.noreply.github.com>
2024-03-13 11:23:42 +0800

    Update fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -15,57 +15,22 @@ import string
 # 安裝或升級 openai 庫
 os.system('pip install openai --upgrade')
 
+# 使用 curl 下載 rocma_qa.json 文件
+os.system('curl -o good.json -L https://raw.githubusercontent.com/ian881123/linebot_openai/master/good.json')
+
 # 定義客戶端
 client = OpenAI() 
 
-# 列出文件
-client.files.list()
-
-# 創建 fine-tuning 作業
-client.fine_tuning.jobs.create(
-  training_file="file-O8h2en23zswFTj9rTDrmjKBP", 
-  model="ft:gpt-3.5-turbo-0125:rocmacis::9265dh12", 
-  hyperparameters={
-    "n_epochs":7
-  }
+# 創建 fine-tune 文件
+client.files.create(
+  file=open("good.json", "rb"),
+  purpose='fine-tune'
 )
-
-# 列出 fine-tuning 作業
-client.fine_tuning.jobs.list(limit=10)
-
-# 檢索 fine-tuning 作業事件
-client.fine_tuning.jobs.retrieve("ftjob-BPOAhbi8SDDHqqW0oKW3301a")
-
-# 列出 fine-tuning 作業事件
-client.fine_tuning.jobs.list_events(fine_tuning_job_id="ftjob-BPOAhbi8SDDHqqW0oKW3301a", limit=10)
-
-# 創建聊天完成
-completion = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::925K80yW",
-  messages=[
-    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "在陸軍軍官學校，學生轉學到其他軍校,須經何單位核定?"}
-  ]
-)
-
-print(completion.choices[0].message.content)
-
-# 創建帶有 fine-tuned 模型的聊天完成
-completion2 = client.chat.completions.create(
-  model="ft:gpt-3.5-turbo-0125:rocmacis::9265dh12",
-  messages=[
-    {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
-    {"role": "user", "content": "在陸軍軍官學校，學生轉學到其他軍校,須經何單位核定?"}
-  ]
-)
-
-print(completion2.choices[0].message.content)
-
 
 # 定義函數 GPT_response，接收文字並使用 fine-tuned 模型生成回應
 def GPT_response(text):
     response = client.chat.completions.create(
-        model="ft:gpt-3.5-turbo-0125:rocmacis::9265dh12",
+        model="ft:gpt-3.5-turbo-0125:rocmacis::929IOM6d",
         messages=[
             {"role": "system", "content": "你扮演一名陸軍軍官學校的客服"},
             {"role": "user", "content": text}
